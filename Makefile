c99937d906b80f76f7d21daab457cfcfec013eab
Peter Fabinski <peter@peterfab.com>
2022-01-17 01:39:45 -0500

    Add circle growth algorithm

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC=clang
-CFLAGS=-Wall -Wextra -pedantic -lpng -g -fsanitize=address
+CFLAGS=-Wall -Wextra -pedantic -lpng -fms-extensions -Wno-microsoft-anon-tag
+OPTFLAGS=-O3
+DEBUGFLAGS=-g -fsanitize=address
 NAME=circlefit
 
 .PHONY: all
 all: $(NAME)
 
 $(NAME): $(NAME).c
-	$(CC) -o $@ $< $(CFLAGS)
+	$(CC) -o $@ $< $(CFLAGS) $(OPTFLAGS)
 
 .PHONY: clean
 clean:
@@ -15,4 +17,8 @@ clean:
 .PHONY: run
 run: $(NAME)
 	maim -u | ./$(NAME) | i3lock --raw 3840x1130:rgb --image /dev/stdin
+
+.PHONY: png
+png: $(NAME)
+	maim -u | ./$(NAME) | convert -size 3840x1130 -depth 8 RGB:- test.png
 
--- circlefit.c
+++ circlefit.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <time.h>
 #include <png.h>
+
+#define SQUARE(x) ((x) * (x))
 
 typedef struct {
     uint8_t r;
@@ -11,11 +15,23 @@ typedef struct {
 } color;
 typedef color pixel;
 
-typedef struct {
+typedef struct circle_t {
     int x;
     int y;
     int r;
 } circle;
+
+typedef struct {
+    union {
+        struct circle_t; // MS extension
+        circle cir;
+    };
+    bool alive;
+} box;
+
+int boxes_size;
+int nboxes;
+box *boxes;
 
 png_image orig;
 pixel *origbuf;
@@ -74,6 +90,7 @@ void fill_circle_octant_points(int cx, int cy, int x, int y, color c) {
 
 // Bresenham Circle Drawing Algorithm
 // https://funloop.org/post/2021-03-15-bresenham-circle-drawing-algorithm.html
+// CAUTION: No bounds checking
 void draw_circle(bool fill, circle cir, color col) {
     // Calculation coordinates are based on (0, 0) at center, math polarity.
     // Start in standard position.
@@ -93,7 +110,7 @@ void draw_circle(bool fill, circle cir, color col) {
         draw_circle_octant_points(cir.x, cir.y, x, y, col);
 
     while (x > y) {
-        if (F < 0) {
+        if (F <= 0) {
             // Northwest would go too far inside the circle, go north instead. X remains the same.
             // Update F: increases by dN
             F += dN;
@@ -117,6 +134,39 @@ void draw_circle(bool fill, circle cir, color col) {
     }
 }
 
+void draw_box(box *b, color fill, color edge) {
+    draw_circle(true, b->cir, fill);
+    draw_circle(false, b->cir, edge);
+}
+
+// Will these two circles collide if both grow by pad?
+bool circles_collide(circle *a, circle *b, int pad) {
+    // squared distance between circle centers
+    int centers = SQUARE(b->x - a->x) + SQUARE(b->y - a->y);
+    // squared sum of radii
+    int radii = SQUARE(a->r + b->r + pad);
+    return (centers < radii);
+}
+
+// Will this box be legal if it grows by pad?
+bool box_legal(box *a, int pad) {
+    if (a->x - a->r - pad < 0 ||
+        a->y - a->r - pad < 0 ||
+        (unsigned int)(a->x + a->r + pad) >= orig.width || // TODO an extra pad?
+        (unsigned int)(a->y + a->r + pad) >= orig.height   // ^
+    ) {
+        return false;
+    }
+
+    for (int i = 0; i < nboxes; i++) {
+        box *b = &boxes[i];
+        if ((a != b) && circles_collide(&a->cir, &b->cir, pad)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void read_image(png_image *image, pixel **buf) {
     image->version = PNG_IMAGE_VERSION;
     image->opaque = NULL;
@@ -130,36 +180,90 @@ void read_image(png_image *image, pixel **buf) {
 }
 
 int main(void) {
+
+    // TODO options
+    // count, border color, spacing, growby etc
+
+    int maxalive = 100;    // max number of live boxes at a time
+    int maxcount = 65535; // max total number of boxes
+    int padding = 1;      // padding between boxes and on edges
+    int growby = 1;       // amount to increase radius each iteration
+    int minradius = 2;    // minimum radius of a circle
+    color edgecol = {0x30, 0x30, 0x30}; // border color of boxes
+
     read_image(&orig, &origbuf);
     outbuf = calloc(orig.width * orig.height, sizeof(pixel));
 
-    /*
-    for (int x = 0; x < orig.width; x++) {
-        for (int y = 0; y < orig.height; y++) {
-            color c = getpixel(x, y);
-            c.r = 255 - c.r;
-            c.g = 255 - c.g;
-            c.b = 255 - c.b;
-            putpixel(x, y, c);
+    srand(time(NULL));
+
+    boxes_size = 2 * maxalive;
+    boxes = calloc(boxes_size, sizeof(*boxes));
+    nboxes = 0;
+
+    int nalive = 0;
+    bool finished = false;
+
+    while (!finished) {
+        // grow boxes if possible
+        for (int i = 0; i < nboxes; i++) {
+            box *b = &boxes[i];
+
+            if (!b->alive) {
+                // don't keep growing, it's already dead
+            } else if (!box_legal(b, growby + padding)) {
+                // can't grow anymore, make it dead
+                b->alive = false;
+                nalive--;
+            } else {
+                // grow the box
+                b->r += growby;
+            }
+        }
+
+        // add new boxes if needed
+        while (nalive < maxalive) {
+            if (boxes_size <= nboxes) {
+                // need to reallocate
+                boxes_size = (1.5 * boxes_size) + nboxes;
+                boxes = realloc(boxes, boxes_size * sizeof(*boxes));
+                if (!boxes) {
+                    fprintf(stderr, "Failed to allocate memory for %d boxes\n",
+                            boxes_size);
+                    exit(ENOMEM);
+                }
+            }
+
+            // try to add a new box 100 times
+            box *b = &boxes[nboxes];
+            for (int i = 0; i < 100; i++) {
+                b->x = padding + (rand() % (orig.width - 2*padding));
+                b->y = padding + (rand() % (orig.height - 2*padding));
+                b->r = minradius;
+
+                if (box_legal(b, padding)) {
+                    // successfully found a spot
+                    b->alive = true;
+                    nboxes++;
+                    nalive++;
+                    break;
+                }
+            }
+            if (!b->alive || nboxes >= maxcount) {
+                // unable to find a new box to add, or reached max
+                finished = true;
+                break;
+            }
         }
     }
-    */
 
-    circle cir;
-    cir.x = 500;
-    cir.y = 500;
-    cir.r = 20;
+    // draw boxes
+    for (int i = 0; i < nboxes; i++) {
+        box *b = &boxes[i];
+        //putpixel(b->x, b->y, (color){0xff, 0xff, 0x00});
+        draw_box(b, getpixel(b->x, b->y), edgecol);
+    }
 
-    color col;
-    col.r = 0xff;
-    col.g = 0x00;
-    col.b = 0x00;
-
-    draw_circle(true, cir, col);
-
-    col.g = 0xff;
-    draw_circle(false, cir, col);
-
+    // TODO libpng output to file (optionally)
     fwrite(outbuf, sizeof(pixel), orig.width * orig.height, stdout);
 
     free(origbuf);
