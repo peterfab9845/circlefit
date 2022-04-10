05ef7abbddb990aa81c12019349e4cb5b96484bf
Peter Fabinski <git@peterfab.com>
2022-04-09 20:19:45 -0400

    Add input format tests

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ CFLAGS=-Wall -Wextra -pedantic -lpng -lnsbmp -fms-extensions -Wno-microsoft-anon
 OPTFLAGS=-O3
 DEBUGFLAGS=-g -fsanitize=address
 NAME=circlefit
-IMAGES=out.png test.png test.bmp
+IMAGES=*.bmp *.png *.asd
+TESTS=test.sh
 
 CFLAGS += $(OPTFLAGS)
 
@@ -24,23 +25,9 @@ install: $(NAME)
 
 .PHONY: lock
 lock: $(NAME)
-	maim -u | ./$(NAME) | i3lock --raw 3840x1130:rgb --image /dev/stdin
+	maim -u -f bmp | ./$(NAME) | i3lock --raw 3840x1130:rgb --image /dev/stdin
 
-.PHONY: pngstdio
-pngstdio: $(NAME)
-	maim -u | ./$(NAME) | convert -size 3840x1130 -depth 8 RGB:- out.png
-
-.PHONY: pngfile
-pngfile: $(NAME)
-	maim -u test.png
-	./$(NAME) | convert -size 3840x1130 -depth 8 RGB:- out.png
-
-.PHONY: bmpstdio
-bmpstdio: $(NAME)
-	maim -u -f bmp | ./$(NAME) | convert -size 3840x1130 -depth 8 RGB:- out.png
-
-.PHONY: bmpfile
-bmpfile: $(NAME)
-	maim -u test.bmp
-	./$(NAME) | convert -size 3840x1130 -depth 8 RGB:- out.png
+.PHONY: test
+test: $(NAME) $(TESTS)
+	NAME=$(NAME) ./$(TESTS)
 
---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+testno=1
+
+# INPUT FORMATS
+
+echo "Test ${testno}: BMP from stdin, bmp format default"
+maim -u -f bmp | ./${NAME} | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: BMP from stdin, bmp format specified"
+maim -u -f bmp | ./${NAME} -f bmp | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: BMP from .bmp file, bmp format guessed/default"
+maim -u input.bmp; ./${NAME} -i input.bmp | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: BMP from .bmp file, bmp format specified"
+maim -u input.bmp; ./${NAME} -i input.bmp -f bmp | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: BMP from .asd file, bmp format specified"
+maim -u -f bmp input.asd; ./${NAME} -i input.asd -f bmp | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: PNG from stdin, png format specified"
+maim -u -f png | ./${NAME} -f png | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: PNG from .png file, png format guessed"
+maim -u input.png; ./${NAME} -i input.png | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+echo "Test ${testno}: PNG from .asd file, png format specified"
+maim -u -f png input.asd; ./${NAME} -i input.asd -f png | convert -size 3840x1130 -depth 8 RGB:- out${testno}.png
+testno=$((testno+1))
+
+# OUTPUT FORMATS
+
+# TODO
+
